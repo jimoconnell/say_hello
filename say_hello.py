8c76585dfc444d06ac707f50efa0f431bd7d9756
Jim O'Connell <jim@mmdc.net>
2018-06-20 16:21:58 -0400

    Initial Commit

---
+++ say_hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import gtts
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("say", help="Speak the provided phrase")
+args = parser.parse_args()
+
+mytext = args.say
+  if not mytext:
+        mytext = "You didn't tell me what to say!"
+        
+tts = gtts.gTTS(mytext, lang='en')
+tts.save("/tmp/tmp.mp3")
+os.system("omxplayer /tmp/tmp.mp3")
